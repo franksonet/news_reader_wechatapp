4a71841e45e0746dc41f560a0d1e55af2d6be4ff
franksonet <frankso.net@gmail.com>
2018-08-09 07:02:41 +0800

    News List implemented

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,67 @@
+const categoryMap = {
+  '国内': 'gn',
+  '国际': 'gj',
+  '财经': 'cj',
+  '娱乐': 'yl',
+  '军事': 'js',
+  '体育': 'ty',
+  '其他': 'other'
+}
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    categoryList: ['国内','国际', '军事','科技', '亲子', '汽车', '娱乐', '历史'],
+    categoryList: Object.keys(categoryMap),
+    currentCategory: '国内',
+    newsList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    // console.log('Getting the news list of defualt category')
+    this.getNewslist()
   },
 
   // When the news category item is tapped:
   onTapTabItem: function(event){
     // console.log(event)
-    let result = event.currentTarget
-    console.log(result.dataset.category)
+    let tabSelected = event.currentTarget.dataset.category
+    console.log('Category selected: ' + tabSelected)
+    console.log(categoryMap[tabSelected])
+    this.setData({
+      currentCategory: categoryMap[tabSelected]
+    })
+  },
+
+  //Get the news list of the selected category
+  getNewslist(callback) {
+    console.log('Getting the news list of the selected category: ' + this.data.currentCategory)
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: categoryMap[this.data.currentCategory]
+      },
+      success: res => {
+        let newsList = res.data.result
+        console.log(newsList)
+        // for (var news in newsList){
+        //   let date = news.date
+        //   news.date = date.slice(11,16)
+        // }
+        this.setData({
+          newsList: newsList
+        })
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
   },
 
   /**
